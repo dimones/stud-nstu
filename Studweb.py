bb6a8cee1092508f34994a8cb8f10d651d11a15e
KarasevAlex <Karasev_a_e@mail.ru>
2017-02-12 23:24:25 +0700

    Start

---
+++ Studweb.py
@@ -0,0 +1,16 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+
+@app.route('/garage')
+def hello_world():
+    return render_template("garage.html")
+
+@app.route('/')
+def hello_world_1():
+    return render_template("onir.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
