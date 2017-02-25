a8ba288879cfd74b105ffd46a62d2f9a93528536
Dmitriy <Dimones951@yandex.ru>
2017-02-25 14:31:17 +0700

    Remove nosetests

--- tests/test_studNSTU.py
+++ tests/test_studNSTU.py
@@ -1,12 +1,17 @@
 from unittest import TestCase
 from Studweb import *
-
+import unittest
 class TestStudNSTU(TestCase):
     s = StudNSTU()
     def test_run(self):
         try:
             self.s.app.test_client()
             self.assertTrue(True)
+            
         except Exception as e:
             print(e)
             self.fail()
+
+
+if __name__ == '__main__':
+    unittest.main()
