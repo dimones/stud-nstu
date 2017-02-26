4365af433afa1fd9a08e60f32bd0d778e85a8f6d
Dmitriy <Dimones951@yandex.ru>
2017-02-26 13:02:03 +0700

    Hi Hitler

--- API/Admin.py
+++ API/Admin.py
@@ -1,7 +1,5 @@
 from flask import *
 from . import api
 from .Auther import *
-@api.route('/api/admin/auth', methods=['POST'])
-def admin_auth():
-    return AdminAuther(request.form['username'],request.form['password'],request.form['device_id']).auth_user()
+
 
