581ac864f39d624a6c06ca5204929fb849a37d5a
khalfina_galina <khalfina.galina@gmail.com>
2017-04-23 08:00:14 +0600

    conferences

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp'),
     mini = require('gulp-minify-css'),
-    concat = require('gulp-concat-css');
+    concat = require('gulp-concat-css'),
+	watch = require('gulp-watch');
   
 gulp.task('minify', function () {
   gulp.src('static/css/forgulp/*.css')
       .pipe(concat('allstyles.css'))
       .pipe(mini())
       .pipe(gulp.dest('static/css'))
+});
+gulp.task('watch', function(){
+    watch(['static/css/forgulp/*.css'], function(event, cb) {
+        gulp.start('minify');
+    });
 });
